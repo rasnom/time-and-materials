77eb4bc320becac73e4c148dc3aa18be1dacd1b8
Ransom <ransom.byers@gmail.com>
2018-04-12 17:07:43 -0700

    Add creation validations for Timesheet

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,2 +1,5 @@
 class Timesheet < ApplicationRecord
+	validates :employee_id, presence: true
+	validates :pay_period_id, presence: true
+
 end
--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -2,8 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Timesheet, type: :model do
   
-	it 'is valid with valid attribues' do
-		expect(Timesheet.new).to be_valid
+	describe 'creation validations:' do
+		it 'is valid with a pay_period and employee' do
+			new_sheet = Timesheet.new(pay_period_id: 12, employee_id: 99)
+			expect(new_sheet.valid?).to be true
+		end
+
+		it 'starts with an initial total_hours of zero' do
+			expect(Timesheet.new.total_hours).to eq 0
+		end
+
+		it 'is not valid without a pay_period' do
+			new_sheet = Timesheet.new(employee_id: 101)
+			expect(new_sheet.valid?).to be false
+		end
+
+		it 'is not valid without an employee' do
+			new_sheet = Timesheet.new(pay_period_id: 4)
+			expect(new_sheet.valid?).to be false
+		end
 	end
+
 
 end
